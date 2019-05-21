3a18c0da3439dc48243feaac3828698891e7773b
SasankG <sasankgraju@gmail.com>
2019-05-20 21:06:36 -0400

    image upload fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +9,6 @@ import (
 	"github.com/rs/cors"
 )
 
-// Define the routes
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -18,37 +16,45 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
-	// Change route to get mux.Vars and also to accept Multipart content-type
-	w.Header().Set("Content-Type", "application/json")
+
+	//w.Header().Set("Content-Type", "multipart/form-data")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	fmt.Println("Connected -> api route")
-
-	// The response sending
-	sample := "connected, this is a JSON"
-	jsonsample, err := json.Marshal(sample)
-	if err != nil {
-		log.Fatal(err)
-	}
-	json.NewEncoder(w).Encode(jsonsample)
 	fmt.Fprintf(w, "connected to api")
+
+	// r.FormFile automatically calls parsemultipartform, we are doing it to double check
+	r.ParseMultipartForm(32 << 20)
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		fmt.Println(err)
+	}
+	file, _, err := r.FormFile("myImage")
+	if err != nil {
+		log.Print(err)
+		fmt.Println("failed to retrieve image")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else if err == nil {
+		fmt.Println("Success, image upload was successful")
+		fmt.Println(file)
+	}
+
+	defer file.Close()
 }
 
 func main() {
 
 	r := mux.NewRouter()
 
-	// Route handlers
 	r.HandleFunc("/", index).Methods("GET")
-	// Initially testing GET, Change to POST after
 	r.HandleFunc("/api", api).Methods("GET", "POST", "OPTIONS")
 
+	// temp cors fix
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"}, // All origins
+		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"GET", "POST"},
 	})
 
-	// Start the server
 	fmt.Println("Server started on PORT 8080")
 	log.Fatal(http.ListenAndServe(":8080", c.Handler(r)))
 }
