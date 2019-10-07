5bfe6013098e4e73f7dab3ca7315121cc91f15f1
SasankG <sasankgraju@gmail.com>
2019-10-07 13:14:59 -0400

    slight refactor + README

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 ## Exif Orientation Image Formatter
 
-- React front end and Go backend
+### This application reorients photographs / images based off of EXIF meta data
+
+#### About
+- The **EXIF Orientation Formatter** extracts an images meta data (learn more here: https://photographylife.com/what-is-exif-data) and returns an image with its corrected orientation
+
+<p align = 'center'>
+    <img src="./screenshots/exifscreenshot.png" width="40%">
+</p>
+
+- The server is written in Go and utilizes rwcarlsen's EXIF decoding package to extract meta data(https://github.com/rwcarlsen/goexif/) 
+
+### Software
+- **React** The interface is built on React
+- **Go** The server is written in Go 
+
+### Author
+- **Sasank Ganapathiraju**
--- main.go
+++ main.go
@@ -21,26 +21,20 @@ func api(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	fmt.Println("Connected -> api route")
+	log.Println("Connected -> api")
 
-	// obtain image from form data
 	r.ParseMultipartForm(32 << 20)
 	if err := r.ParseMultipartForm(32 << 20); err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 
 	file, multipartFileHeader, err := r.FormFile("myImage")
 	if err != nil {
 		log.Print(err)
-		fmt.Println("failed to retrieve image")
+		log.Println("failed to retrieve image")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	// logs -------
-	fmt.Println("Success, image upload was successful")
-	fmt.Println(reflect.TypeOf(file))
-	// ------------
 
 	buf := bytes.NewBuffer(nil)
 	if _, err := io.Copy(buf, file); err != nil {
@@ -49,7 +43,6 @@ func api(w http.ResponseWriter, r *http.Request) {
 		log.Print(reflect.TypeOf(buf))
 	}
 
-	// open image and create a file on server
 	imageOutput, err := os.Create(multipartFileHeader.Filename)
 	if err != nil {
 		log.Print(err)
@@ -58,20 +51,13 @@ func api(w http.ResponseWriter, r *http.Request) {
 	_, err = imageOutput.Write(buf.Bytes())
 	if err != nil {
 		log.Print(err)
-	} else {
-		log.Print(reflect.TypeOf(imageOutput))
-		namer := imageOutput.Name()
-		log.Print(namer)
 	}
 
-	// Save
-	// open the file for reading
 	imageSave, err := os.Open(imageOutput.Name())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// save file in ./images
 	dir := "./images"
 	dst, err := os.Create(filepath.Join(dir, filepath.Base(util.NameGen(4)+imageSave.Name())))
 	if err != nil {
@@ -82,21 +68,19 @@ func api(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	log.Println("File Saved")
 	defer imageSave.Close()
 	defer dst.Close()
 
-	// transform image
 	util.Transform(imageOutput.Name())
 
 	defer imageOutput.Close()
 	defer file.Close()
 
-	// return transformed image to client
 	var files []string
 	var getImg string
 
-	// walk through transformed image folder
 	root := "./testingsave"
 	errs := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		files = append(files, path)
@@ -105,14 +89,13 @@ func api(w http.ResponseWriter, r *http.Request) {
 	if errs != nil {
 		panic(err)
 	}
+
 	for _, file := range files {
 		if filepath.Ext(file) == ".jpg" {
 			getImg = file
-			log.Println(getImg)
 		}
 	}
 
-	// open image to send
 	finalImage, err := os.Open(getImg)
 	if err != nil {
 		log.Fatal(err)
@@ -120,25 +103,19 @@ func api(w http.ResponseWriter, r *http.Request) {
 
 	defer finalImage.Close()
 
-	// create buffer for image to send
 	imgInfo, _ := finalImage.Stat()
 	var finalImageSize = imgInfo.Size()
 	finalBuf := make([]byte, finalImageSize)
 
-	// read final image into a biffer
 	fReader := bufio.NewReader(finalImage)
 	fReader.Read(finalBuf)
 
-	// convert to base 64
 	imgBase64Str := base64.StdEncoding.EncodeToString(finalBuf)
 
-	// send to client base64
 	fmt.Fprintf(w, imgBase64Str)
 
-	// delete image
 	os.Remove(imageOutput.Name())
 
-	// clear testingsave
 	os.RemoveAll(getImg)
 
 	return
@@ -149,13 +126,17 @@ func main() {
 
 	r := mux.NewRouter()
 
-	// handle app
 	buildHandler := http.FileServer(http.Dir("./client/build"))
 	r.PathPrefix("/").Handler(buildHandler).Methods("GET")
 
 	r.HandleFunc("/api", api).Methods("GET", "POST", "OPTIONS")
 
-	// configure server
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8080",
@@ -163,8 +144,7 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	// serve
-	fmt.Println("Server started on PORT 8080")
+	log.Println("Server started on PORT" + port)
 	log.Fatal(srv.ListenAndServe())
 
 }
